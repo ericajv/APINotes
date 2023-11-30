a1820dd348353bcea7694bf2e431fd7390ad9f67
Érica Vieira <ericaj.vieira13@gmail.com>
2023-11-29 23:33:50 -0300

    Funcionalidade de atualizar dados do usuário

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -9,5 +9,6 @@ const usersRoutes = Router();
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
