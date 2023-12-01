b54e1b5bbc367f87cab29e10cf412128e2084215
Érica Vieira <ericaj.vieira13@gmail.com>
2023-12-01 14:54:48 -0300

    Cria migrations para Users, Notes, Tags e links

--- src/server.js
+++ src/server.js
@@ -15,18 +15,18 @@ app.use(routes);
 
 
 app.use((error, request, response, next) => {
-if (error instanceof AppError){
-    return response.status(error.statusCode).json({
-        status:"error",
-        messege: error.message
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            messege: error.message
 
-    });
-} 
+        });
+    }
     console.error(error);
 
     return response.status(500).json({
-        status:"error",
-        message:"Internal server error",
+        status: "error",
+        message: "Internal server error",
     });
 
 
